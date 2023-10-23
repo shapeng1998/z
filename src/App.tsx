098c26a91f186647cce079141eb0ac61709670a4
shapeng1998 <me@pengsha.dev>
2023-10-23 11:42:23 +0800

    chore: update deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ImageViewer from './components/ImageViewer/ImageViewer';
 import testImage from './assets/img.jpg';
 
 function App() {
-  return <ImageViewer src={testImage} type="framer-motion" />;
+  return <ImageViewer src={testImage} type="vanilla-react" />;
 }
 
 export default App;
