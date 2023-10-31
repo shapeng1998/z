d98e3eb2db6c0378e87a5346695e90b87b5a69e5
shapeng1998 <me@pengsha.dev>
2023-10-31 21:09:13 +0800

    wip: feat: eslint prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.cjs', 'postcss.config.js'],
   parser: '@typescript-eslint/parser',
