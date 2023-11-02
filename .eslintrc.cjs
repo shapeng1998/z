15acaf6fd8e2b3ccde84c5b327ce83058a21d1c2
shapeng1998 <me@pengsha.dev>
2023-11-02 14:17:17 +0800

    chore: add eslint regexp rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:regexp/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.cjs', 'postcss.config.js'],
   parser: '@typescript-eslint/parser',
