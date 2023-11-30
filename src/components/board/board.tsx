367b891376f2717076d854dad71de63c378ec308
Peng Sha <me@pengsha.dev>
2023-11-30 10:06:08 +0800

    chore: clear all

--- src/components/board/board.tsx
+++
@@ -1,9 +0,0 @@
-export const Board = () => {
-  return (
-    <div className="grid grid-cols-4 gap-2 p-2">
-      {Array.from({ length: 16 }, (_, index) => (
-        <div key={index} className="h-10 w-10 bg-slate-300"></div>
-      ))}
-    </div>
-  );
-};
--- src/components/image-viewer/image-viewer.constants.ts
+++
@@ -1,1 +0,0 @@
-export const AXIS_THRESHOLD = 10;
