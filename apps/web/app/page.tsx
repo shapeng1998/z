d6641735ea7b48811cb71c6f6b7e7929a5596fae
Peng Sha <me@pengsha.dev>
2024-01-07 15:51:41 +0800

    feat: init with nextjs template

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@z/ui/card";
+import { Code } from "@z/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@z/ui/button";
 
 function Gradient({
   conic,
